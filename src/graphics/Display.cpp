c627807c51ed1c149fe5b6d50943d2634da31832
mlugg <mlugg@mlugg.co.uk>
2019-11-27 10:40:55 +0000

    Basic SDL initialization

--- src/graphics/Display.cpp
+++ src/graphics/Display.cpp
@@ -1,6 +1,31 @@
 #include "Display.hpp"
 #include <SDL2/SDL.h>
+#include <stdexcept>
 
-void Display::init() {
-  SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+SDL_Surface *Display::m_surface;
+SDL_Window *Display::m_window;
+
+void Display::init(const char *title, int width, int height) {
+  if (SDL_Init(SDL_INIT_VIDEO)) {
+    throw std::runtime_error(SDL_GetError());
+  }
+
+  m_window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+
+  if (!m_window) {
+    throw std::runtime_error(SDL_GetError());
+  }
+
+  m_surface = SDL_GetWindowSurface(m_window);
+
+  // Test
+  SDL_FillRect(m_surface, NULL, 0xFFFFFF);
+  update();
+  SDL_Delay(2000);
 }
+
+void Display::update() {
+  SDL_UpdateWindowSurface(m_window);
+}
+
+// vim: et sw=2
--- src/graphics/Display.hpp
+++ src/graphics/Display.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
 class Display {
 public:
-  static void init();
+  static void init(const char *title, int width, int height);
+  static void update();
+  static SDL_Surface *m_surface;
+private:
+  static SDL_Window *m_window;
 };
+
+// vim: et sw=2
