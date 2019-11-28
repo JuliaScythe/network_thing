af2f55d0bc198ee797bed5ba4c4ae86c46a51987
mlugg <mlugg@mlugg.co.uk>
2019-11-28 15:34:48 +0000

    Add test SVG loading code

--- src/graphics/Display.cpp
+++ src/graphics/Display.cpp
@@ -1,11 +1,12 @@
 #include "Display.hpp"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdexcept>
+#include <cmath>
 
-SDL_Surface *Display::m_surface;
-SDL_Window *Display::m_window;
+#define TAU 6.28318
 
-void Display::init(const char *title, int width, int height) {
+Display::Display(const char *title, int width, int height) {
   if (SDL_Init(SDL_INIT_VIDEO)) {
     throw std::runtime_error(SDL_GetError());
   }
@@ -16,16 +17,33 @@ void Display::init(const char *title, int width, int height) {
     throw std::runtime_error(SDL_GetError());
   }
 
-  m_surface = SDL_GetWindowSurface(m_window);
+  m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
+
+  SDL_SetRenderDrawColor(m_renderer, 255, 0, 0, 255);
+  SDL_RenderClear(m_renderer);
 
   // Test
-  SDL_FillRect(m_surface, NULL, 0xFFFFFF);
+  SDL_SetRenderDrawColor(m_renderer, 0, 0, 255, 255);
+  SDL_RWops *x = SDL_RWFromFile("./res/node_basic.svg", "r");
+  SDL_Surface *img = IMG_LoadSVG_RW(x);
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(m_renderer, img);
+
+  SDL_Rect dst = {.x=100, .y=100, .w=200, .h=200};
+
+  SDL_RenderCopy(m_renderer, texture, NULL, &dst);
+
   update();
+
   SDL_Delay(2000);
 }
 
 void Display::update() {
-  SDL_UpdateWindowSurface(m_window);
+  SDL_RenderPresent(m_renderer);
 }
 
-// vim: et sw=2
+Display::~Display() {
+  SDL_DestroyWindow(m_window);
+  SDL_Quit();
+}
+
+// vim: sw=2 et
--- src/graphics/Display.hpp
+++ src/graphics/Display.hpp
@@ -4,11 +4,15 @@
 
 class Display {
 public:
-  static void init(const char *title, int width, int height);
-  static void update();
-  static SDL_Surface *m_surface;
+  Display(const char *title, int width, int height);
+  ~Display();
+  void update();
+  SDL_Renderer *m_renderer;
+  SDL_GLContext m_gl;
+  void circle(unsigned x, unsigned y, unsigned radius);
+  void filledCircle(unsigned x, unsigned y, unsigned radius);
 private:
-  static SDL_Window *m_window;
+  SDL_Window *m_window;
 };
 
 // vim: et sw=2
