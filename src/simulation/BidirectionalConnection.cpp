07dc65f65e965530320b24f83e1ff6dc350bba0c
Jake Irvine <me@jakeirvine.com>
2019-12-03 09:25:06 +0000

    Fix bidirectional connections, like actually make them do something

--- src/simulation/BidirectionalConnection.cpp
+++ src/simulation/BidirectionalConnection.cpp
@@ -9,3 +9,17 @@ void BidirectionalConnection::sendPacket(Packet &packet, Node *node) {
 	throw new std::runtime_error("Invalid source node!");
   }
 }
+
+void BidirectionalConnection::doTick() {
+  Connection::doTick(); // Ensure the forward packets are moved forward
+
+  // Move the reverse packets forward
+  for (unsigned i = 0; i < mReversePackets.size(); i++) {
+    mReversePackets[i].progress += deltaProgress;
+    if (mReversePackets[i].progress > 1.0f) {
+      auto x = mReversePackets.begin() + (i--);
+      mReversePackets.erase(x);
+    }
+  
+
+}
